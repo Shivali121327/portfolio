4843b4063a9decdf61f8446ac6ecf6be20f8451c
shivali <chansoriyashivali@gmail.com>
2015-07-01 11:58:53 +0530

    Added wow js animation in the skills

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
-
-$("#js-rotating").Morphext({
+$(document).ready(function() {
+  $("#js-rotating").Morphext({
     // The [in] animation type. Refer to Animate.css for a list of available animations.
     animation: "bounceIn",
     // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
@@ -9,4 +9,6 @@ $("#js-rotating").Morphext({
     complete: function () {
         // Called after the entrance animation is executed.
     }
+  });
+  new WOW().init();
 });
