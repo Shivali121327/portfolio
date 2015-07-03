7abd44b2746befccd5b6c45da4a20cf595980d40
shivali <chansoriyashivali@gmail.com>
2015-07-03 11:28:06 +0530

    Added new js animation 'Morphtext' in the Home page

--- js/home.js
+++ js/home.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-  $("#js-rotating").Morphext({
-    // The [in] animation type. Refer to Animate.css for a list of available animations.
-    animation: "bounceIn",
-    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
-    separator: ",",
-    // The delay between the changing of each phrase in milliseconds.
-    speed: 2000,
-    complete: function () {
-        // Called after the entrance animation is executed.
-    }
-  });
+  $('#demo').Morphext({
+	  // animation settings.
+	  animation: "fadeIn",
+	  // custom separator
+	  separator: ",",
+	  // animation speed
+	  speed: 3000,
+	  // callback
+	  complete: $.noop
+	});
   new WOW().init();
 });
