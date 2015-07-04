f918c373ca22f6aa17390edfa15c3f7ed1d4fbaf
shivali <chansoriyashivali@gmail.com>
2015-07-04 12:07:48 +0530

    Added scrollspy

--- js/home.js
+++ js/home.js
@@ -9,5 +9,15 @@ $(document).ready(function() {
 	  // callback
 	  complete: $.noop
 	});
+	$(function(){
+    $(".nav a").click(function(e){
+      e.preventDefault();
+      $('html,body').scrollTo(this.hash,this.hash); 
+    });
+  });
+	$('body').scrollspy({ target: '#navbar-example' })
+	$('[data-spy="scroll"]').each(function () {
+  var $spy = $(this).scrollspy('refresh')
+  })
 });
 new WOW().init();
